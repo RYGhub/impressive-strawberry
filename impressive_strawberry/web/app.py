82291105bb5025e434483c37700ba797ce9b9f7a
Stefano Pigozzi <me@steffo.eu>
2021-11-12 17:53:36 +0100

    💥 Configure application authentication

--- impressive_strawberry/web/app.py
+++ impressive_strawberry/web/app.py
@@ -1,7 +1,7 @@
 import fastapi
 import pkg_resources
 
-from impressive_strawberry.web.routes.api.application.v1.router import app_router
+from impressive_strawberry.web.routes.api.application.v1.router import router as router_api_application_v1
 
 app = fastapi.FastAPI(
     debug=__debug__,
@@ -10,6 +10,6 @@ app = fastapi.FastAPI(
     version=pkg_resources.get_distribution("impressive_strawberry").version,
 )
 app.include_router(
-    app_router,
+    router_api_application_v1,
     prefix="/api/application/v1",
 )
--- impressive_strawberry/web/routes/api/application/v1/router.py
+++ impressive_strawberry/web/routes/api/application/v1/router.py
@@ -1,67 +1,72 @@
-from uuid import UUID
-
 import fastapi.routing
 from sqlalchemy.orm import Session
 
 from impressive_strawberry.database import tables
 from impressive_strawberry.database.deps import dep_session
 from impressive_strawberry.web import response_schema, models, responses
-from impressive_strawberry.web.quick import quick_create, quick_retrieve, quick_update
+from impressive_strawberry.web.auth.deps import dep_application
+from impressive_strawberry.web.quick import quick_create, quick_update
 
-app_router = fastapi.routing.APIRouter(
+router = fastapi.routing.APIRouter(
     tags=[
-        "application",
+        "Application",
     ],
 )
 
 
-# FIXME: Either add a check to enforce only the currently logged in application to be read or remove the identifier.
-
-
-@app_router.get(
-    "/{id_}",
-    response_model=models.ApplicationFull,
-    responses={
-        **response_schema.NOT_FOUND
-    },
-)
-async def application_get(
-        id_: UUID,
-        session: Session = fastapi.Depends(dep_session)
-):
-    return quick_retrieve(session=session, table=tables.Application, id=id_)
-
-
-@app_router.post(
+@router.post(
     "/",
+    summary="Create a new application, and get a token.",
     response_model=models.ApplicationFull,
 )
 async def application_post(
+        *,
         data: models.ApplicationCreate,
         session: Session = fastapi.Depends(dep_session)
 ):
     return quick_create(session, tables.Application(name=data.name, description=data.description, webhook=data.webhook))
 
 
-@app_router.put(
-    "/{id_}",
+@router.get(
+    "/me",
+    summary="Get the application you're authenticating as.",
+    response_model=models.ApplicationFull,
+    responses={
+        **response_schema.UNAUTHORIZED,
+        **response_schema.NOT_FOUND,
+    },
+)
+async def application_get(
+        *,
+        application: tables.Application = fastapi.Depends(dep_application),
+):
+    return application
+
+
+@router.put(
+    "/me",
+    summary="Change the details of the application you're authenticating as.",
     response_model=models.ApplicationFull,
 )
 async def application_put(
-        id_: UUID,
+        *,
+        application: tables.Application = fastapi.Depends(dep_application),
         data: models.ApplicationCreate,
         session: Session = fastapi.Depends(dep_session)
 ):
-    application: tables.Application = quick_retrieve(session=session, table=tables.Application, id=id_)
     return quick_update(session=session, obj=application, data=data)
 
 
-@app_router.delete(
-    "/{id_}",
+@router.delete(
+    "/me",
+    summary="Delete the application you're authenticating as.",
     status_code=204,
 )
-async def application_patch(id_: UUID, session: Session = fastapi.Depends(dep_session)):
-    application: tables.Application = quick_retrieve(session=session, table=tables.Application, id=id_)
+async def application_delete(
+        *,
+        application: tables.Application = fastapi.Depends(dep_application),
+        session: Session = fastapi.Depends(dep_session),
+):
     session.delete(application)
     session.commit()
     return responses.NO_CONTENT
