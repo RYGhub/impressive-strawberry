2926519bb09d4fb401d61f987e53b62b9c995e83
Stefano Pigozzi <me@steffo.eu>
2021-11-12 04:17:41 +0100

    🧹 Remove uvicorn start, as it already exists on `__main__.py`

--- impressive_strawberry/web/app.py
+++ impressive_strawberry/web/app.py
@@ -1,14 +1,9 @@
 import fastapi
 import pkg_resources
-import uvicorn
-import os
 
-from impressive_strawberry.web.routes import app_router
-from impressive_strawberry.database.tables import Base
 from impressive_strawberry.database.engine import engine
-from impressive_strawberry.database.deps import dep_session
-from impressive_strawberry.database.tables import Application
-
+from impressive_strawberry.database.tables import Base
+from impressive_strawberry.web.routes import app_router
 
 app = fastapi.FastAPI(
     debug=__debug__,
@@ -19,6 +14,3 @@ app = fastapi.FastAPI(
 app.include_router(app_router)
 
 Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("IS_PORT")))
