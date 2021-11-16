1212cde50d583a6bde96c10920734e9d81e93093
Nemesis <lorenzo.balugani@gmail.com>
2021-11-16 15:18:09 +0100

    Add group router

---
+++ impressive_strawberry/web/routes/api/group/v1/router.py
@@ -0,0 +1,16 @@
+import fastapi.routing
+from sqlalchemy.orm import Session
+
+from impressive_strawberry.database import tables
+from impressive_strawberry.web import crud
+from impressive_strawberry.web import deps
+from impressive_strawberry.web import models
+from impressive_strawberry.web import responses
+
+router = fastapi.routing.APIRouter(
+    prefix="/api/group/v1",
+    tags=[
+        "Group v1",
+    ],
+)
+
