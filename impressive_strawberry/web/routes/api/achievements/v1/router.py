8580a06e0bb255549d53dc67f070f6c1b591efa0
Nemesis <lorenzo.balugani@gmail.com>
2021-11-18 13:25:38 +0100

    Add basic router

---
+++ impressive_strawberry/web/routes/api/achievements/v1/router.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+
+import fastapi.routing
+from sqlalchemy.orm import Session
+
+from impressive_strawberry.database import tables
+from impressive_strawberry.web import crud
+from impressive_strawberry.web import deps
+from impressive_strawberry.web import models
+from impressive_strawberry.web import responses
+
+router = fastapi.routing.APIRouter(
+    prefix="/api/application/this/group/{group_id}/achievements/v1",
+    tags=[
+        "Achievements v1",
+    ],
+)
