9fa45ec64089530e58e932ac12ef147b13c0d6b5
Stefano Pigozzi <me@steffo.eu>
2021-11-26 08:24:59 +0100

    ✨ Create asyncronous test client fixture `client`

--- impressive_strawberry/web/testing/client.py
+++ impressive_strawberry/web/testing/client.py
@@ -1,5 +1,10 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from impressive_strawberry.web.app import app
 
-client = TestClient(app=app)
+
+@pytest.fixture(scope="package")
+async def client() -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://test"):
+        yield client
