77ad373893a8fb1f5a8b69bf08bcd2ff3403c613
Stefano Pigozzi <me@steffo.eu>
2021-11-26 08:40:56 +0100

    ✨ Create the database session fixture `session`

--- impressive_strawberry/web/testing/conftest.py
+++ impressive_strawberry/web/testing/conftest.py
@@ -1,10 +1,17 @@
 import pytest
 from httpx import AsyncClient
 
+from impressive_strawberry.database.engine import Session
 from impressive_strawberry.web.app import app
 
 
 @pytest.fixture(scope="session")
 async def client() -> AsyncClient:
-    async with AsyncClient(app=app, base_url="http://test"):
-        yield client
+    async with AsyncClient(app=app, base_url="http://test") as c:
+        yield c
+
+
+@pytest.fixture(scope="session")
+def session() -> Session:
+    with Session() as s:
+        yield s
