e673830076a923651aa5b5482a11af22fc41131d
Stefano Pigozzi <me@steffo.eu>
2021-11-26 08:40:40 +0100

    🔧 Make the `client` fixture use the "session" scope

--- impressive_strawberry/web/testing/conftest.py
+++ impressive_strawberry/web/testing/conftest.py
@@ -4,7 +4,7 @@ from httpx import AsyncClient
 from impressive_strawberry.web.app import app
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="session")
 async def client() -> AsyncClient:
     async with AsyncClient(app=app, base_url="http://test"):
         yield client
