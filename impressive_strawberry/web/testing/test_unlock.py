25b441336f82421483c47b6f9eb1478e757fba7f
Stefano Pigozzi <me@steffo.eu>
2021-12-20 17:18:10 +0100

    🧪 Test (somewhat) webhook calls (#18)

--- impressive_strawberry/web/testing/test_unlock.py
+++ impressive_strawberry/web/testing/test_unlock.py
@@ -8,10 +8,11 @@ pytestmark = pytest.mark.asyncio
 
 
 class TestUnlockCreate:
-    async def test_success(self, mocker: pytest_mock.MockerFixture, authenticated_client: httpx.AsyncClient, achievement: tables.Achievement,
-                           user: tables.User):
+    async def test_success(self, mocker: pytest_mock.MockerFixture, authenticated_client: httpx.AsyncClient, application: tables.Application,
+                           achievement: tables.Achievement, user: tables.User):
         true_post = authenticated_client.post
-        mocker.patch("httpx.AsyncClient.post")
+        webhook_post = mocker.AsyncMock()
+        mocker.patch("httpx.AsyncClient.post", webhook_post)
 
         response = await true_post(
             f"/api/unlock/v1/", params={
@@ -20,6 +21,12 @@ class TestUnlockCreate:
                 "user": user.crystal,
             })
         assert response.status_code == 201
+        assert webhook_post.call_count == 1
+        assert webhook_post.call_args.args[0] == application.webhook_url  # ?
+        assert webhook_post.call_args.kwargs["json"].items() >= {
+            "achievement_id": achievement.id,
+            "user_id": user.id,
+        }.items()
 
         data = response.json()
         assert data.items() >= {
@@ -37,15 +44,23 @@ class TestUnlockDelete:
 
 
 class TestDirectUnlockCreate:
-    async def test_success(self, mocker: pytest_mock.MockerFixture, client: httpx.AsyncClient, achievement: tables.Achievement, user: tables.User):
+    async def test_success(self, mocker: pytest_mock.MockerFixture, client: httpx.AsyncClient, application: tables.Application, achievement: tables.Achievement,
+                           user: tables.User):
         true_post = client.post
-        mocker.patch("httpx.AsyncClient.post")
+        webhook_post = mocker.AsyncMock()
+        mocker.patch("httpx.AsyncClient.post", webhook_post)
 
         response = await true_post(f"/api/unlock-direct/v1", params={
             "token": achievement.token,
             "user": user.crystal,
         })
         assert response.status_code == 201
+        assert webhook_post.call_count == 1
+        assert webhook_post.call_args.args[0] == application.webhook_url  # ?
+        assert webhook_post.call_args.kwargs["json"].items() >= {
+            "achievement_id": achievement.id,
+            "user_id": user.id,
+        }.items()
 
         data = response.json()
         assert data.items() >= {
