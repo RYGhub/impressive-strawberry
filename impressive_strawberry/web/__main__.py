eeca43e259e2efa70f85bd6af5e63b0b28553c41
Stefano Pigozzi <me@steffo.eu>
2021-12-28 16:48:19 +0100

    🔧 Run migrations on __main__

--- impressive_strawberry/web/__main__.py
+++ impressive_strawberry/web/__main__.py
@@ -1,11 +1,19 @@
+import logging
 import os
 
+import alembic.config
 import dotenv
+import uvicorn
+
+logging.basicConfig(level="DEBUG")
+log = logging.getLogger(__name__)
 
 dotenv.load_dotenv(".env", override=True)
 dotenv.load_dotenv(".env.local", override=True)
 
-import uvicorn
 from impressive_strawberry.web.app import app
 
+log.info("Running migrations with Alembic...")
+alembic.config.main(argv=["--raiseerr", "upgrade", "head"])
+log.info("Running impressive_strawberry with Uvicorn...")
 uvicorn.run(app, port=int(os.environ["IS_WEB_PORT"]))
