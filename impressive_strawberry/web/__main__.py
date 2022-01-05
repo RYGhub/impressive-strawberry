ce0be064245b54382d806f935bdfca89a262d6d8
Stefano Pigozzi <me@steffo.eu>
2022-01-05 17:25:16 +0100

    ✨ Add CORS middleware

--- impressive_strawberry/web/__main__.py
+++ impressive_strawberry/web/__main__.py
@@ -3,6 +3,7 @@ import os
 
 import alembic.config
 import dotenv
+import fastapi.middleware.cors as cors
 import uvicorn
 
 logging.basicConfig(level="DEBUG")
@@ -15,5 +16,13 @@ from impressive_strawberry.web.app import app
 
 log.info("Running migrations with Alembic...")
 alembic.config.main(argv=["--raiseerr", "upgrade", "head"])
+log.info("Creating CORS middleware...")
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=os.environ["CORS_ALLOW_ORIGINS"].split(" "),
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 log.info("Running impressive_strawberry with Uvicorn...")
+# noinspection PyTypeChecker
 uvicorn.run(app, port=int(os.environ["IS_WEB_PORT"]))
