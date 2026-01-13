8bc67506e7edcb10b99efc77c778db6f752bbfa1
Stefano Pigozzi <me@steffo.eu>
2026-01-13 01:21:26 +0100

    Add main development script

--- impressive_strawberry/web/__main__.py
+++ impressive_strawberry/web/__main__.py
@@ -8,24 +8,28 @@ import uvicorn
 
 from impressive_strawberry.utils import install_general_log_handlers
 
-install_general_log_handlers()
+def main():
+	install_general_log_handlers()
 
-log = logging.getLogger(__name__)
+	log = logging.getLogger(__name__)
 
-dotenv.load_dotenv(".env", override=True)
-dotenv.load_dotenv(".env.local", override=True)
+	dotenv.load_dotenv(".env", override=True)
+	dotenv.load_dotenv(".env.local", override=True)
 
-from impressive_strawberry.web.app import app
+	from impressive_strawberry.web.app import app
 
-log.info("Running migrations with Alembic...")
-alembic.config.main(argv=["--raiseerr", "upgrade", "head"])
-log.info("Creating CORS middleware...")
-app.add_middleware(
-    cors.CORSMiddleware,
-    allow_origins=os.environ["CORS_ALLOW_ORIGINS"].split(" "),
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-log.info("Running impressive_strawberry with Uvicorn...")
-# noinspection PyTypeChecker
-uvicorn.run(app, port=int(os.environ["IS_WEB_PORT"]), host=os.environ["IS_WEB_HOST"], log_config=None)
+	log.info("Running migrations with Alembic...")
+	alembic.config.main(argv=["--raiseerr", "upgrade", "head"])
+	log.info("Creating CORS middleware...")
+	app.add_middleware(
+		cors.CORSMiddleware,
+		allow_origins=os.environ["CORS_ALLOW_ORIGINS"].split(" "),
+		allow_methods=["*"],
+		allow_headers=["*"],
+	)
+	log.info("Running impressive_strawberry with Uvicorn...")
+	# noinspection PyTypeChecker
+	uvicorn.run(app, port=int(os.environ["IS_WEB_PORT"]), host=os.environ["IS_WEB_HOST"], log_config=None)
+
+if __name__ == "__main__":
+	main()
